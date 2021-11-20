9cab1e2383acd7cebb97b4f9f7139d309cd15fee
Eric Lopes <olv.ericlopes@gmail.com>
2021-11-20 11:00:31 -0300

    Fix - better treatment to promotion for my chess game

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,7 +43,12 @@ public class Program {
 				// promotion special move
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					// validating
+					while (!type.equals("B") && !type.equals("N")&& !type.equals("R")&& !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 				
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -143,11 +142,8 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		// defensive programming
-		if (!type.equals("B") 
-				&& !type.equals("N")
-				&& !type.equals("R")
-				&& !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
